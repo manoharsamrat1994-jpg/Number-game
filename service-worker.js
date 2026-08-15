f9e3a7e9da94006274061fb0d4ce3277de70fa3d
manoharsamrat1994-jpg <manoharsamrat1994@gmail.com>
2026-08-16 00:15:52 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "number-game-offline-v1";
+const APP_FILES = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(APP_FILES);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(keys.filter(function(key) {
+                return key !== CACHE_NAME;
+            }).map(function(key) {
+                return caches.delete(key);
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(cachedResponse) {
+            return cachedResponse || fetch(event.request).catch(function() {
+                return caches.match("./index.html");
+            });
+        })
+    );
+});
